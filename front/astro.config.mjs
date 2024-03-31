721e79eb1b50ca9d58554d1d4d9098e165618af2
Jonathan Cruz <jonathan.cruz89@gmail.com>
2024-03-30 20:26:29 -0500

    Add login and register pages

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static'
+  integrations: [tailwind()]
 });
